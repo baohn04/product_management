cee1b2009dc9e6b19a66bd5c14c51d66e66ff445
Hà Nhất Bảo <baohn910@gmail.com>
2025-05-05 22:08:07 +0700

    cloud

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const multer = require("multer");
 const router = express.Router();
-const storageMulter = require("../../helpers/storageMulter.js");
-const upload = multer({ storage: storageMulter() });
+// const storageMulter = require("../../helpers/storageMulter.js");
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware.js");
+const upload = multer();
 
 const controller = require("../../controllers/admin/product.controller.js");
 const validate = require("../../validates/admin/product.validate.js");
@@ -15,6 +16,7 @@ router.get("/create", controller.create);
 router.post(
   "/create",
   upload.single("thumbnail"),
+  uploadCloud.upload,
   validate.createPost,
   controller.createPost
 );
@@ -22,6 +24,7 @@ router.get("/edit/:id", controller.edit);
 router.patch(
   "/edit/:id",
   upload.single("thumbnail"),
+  uploadCloud.upload,
   validate.createPost,
   controller.editPatch
 );
