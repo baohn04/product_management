681f6d4c430aad3e28555c2c4d25f80846e8d4d6
Hà Nhất Bảo <baohn910@gmail.com>
2025-05-21 15:01:14 +0700

    page login logout

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -5,6 +5,7 @@ const productRoutes = require("./product.route.js");
 const productCategoryRoutes = require("./product-category.route.js");
 const roleRoutes = require("./role.route.js");
 const accountRoutes = require("./account.route.js");
+const authRoutes = require("./auth.route.js");
 
 module.exports = (app) => {
   const PATH_ADMIN = systemConfig.prefixAdmin;
@@ -14,4 +15,5 @@ module.exports = (app) => {
   app.use(PATH_ADMIN + "/products-category", productCategoryRoutes);
   app.use(PATH_ADMIN + "/roles", roleRoutes);
   app.use(PATH_ADMIN + "/accounts", accountRoutes);
+  app.use(PATH_ADMIN + "/auth", authRoutes);
 };
---
+++ validates/admin/auth.validate.js
@@ -0,0 +1,15 @@
+module.exports.loginPost = (req, res, next) => {
+  if(!req.body.email) {
+    req.flash("error", "Vui lòng nhập Email!");
+    res.redirect(req.get("Referrer") || "/");
+    return;
+  }
+
+  if(!req.body.password) {
+    req.flash("error", "Vui lòng nhập mật khẩu!");
+    res.redirect(req.get("Referrer") || "/");
+    return;
+  }
+  
+  next(); // Chuyển sang đoạn mã kế tiếp
+}
