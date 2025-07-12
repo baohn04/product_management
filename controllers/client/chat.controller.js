b196c99b5333387cefb799bebb29965bbb250a74
Hà Nhất Bảo <baohn910@gmail.com>
2025-07-12 22:58:24 +0700

    update chat

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,31 +1,13 @@
 const Chat = require("../../models/chat.model");
 const User = require("../../models/user.model");
+const chatSocket = require("../../sockets/client/chat.socket.js");
 
 // [GET] /chat/
 module.exports.index = async (req, res) => {
-  const userId = res.locals.user.id;
-  const fullName = res.locals.user.fullName;
-
   // SocketIO
-  _io.once('connection', (socket) => { // once: chỉ kết nối user 1 lần duy nhất (không cần phải kết nối lại khi load lại trang)
-    socket.on("CLIENT_SEND_MASSAGE", async (content) => {
-      // Lưu vào database
-      const chat = new Chat({
-        user_id: userId,
-        content: content
-      });
-      await chat.save();
-
-      // Trả data về cho client
-      _io.emit("SERVER_RETURN_MASSAGE", {
-        userId: userId,
-        fullName: fullName,
-        content: content
-      });
-    });
-  });
+  chatSocket(res);
   // END SocketIO
-
+  
   // Lấy ra data
   const chats = await Chat.find({
     deleted: false
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,13 +1,29 @@
+import * as Popper from 'https://cdn.jsdelivr.net/npm/@popperjs/core@^2/dist/esm/index.js';
+
+// FileUpLoadWithPreview
+const upload = new FileUploadWithPreview.FileUploadWithPreview('upload-image', {
+  multiple: true,
+  maxFileCount: 6
+});
+// End FileUpLoadWithPreview
+
 // CLIENT_SEND_MASSAGE
 const formSendData = document.querySelector(".chat .inner-form");
 if (formSendData) {
   formSendData.addEventListener("submit", (e) => {
     e.preventDefault();
     const content = e.target.elements.content.value;
+    const images = upload.cachedFileArray || [];
 
-    if (content) {
-      socket.emit("CLIENT_SEND_MASSAGE", content);
+    if (content || images.length > 0) {
+      // Gửi content hoặc ảnh lên server
+      socket.emit("CLIENT_SEND_MASSAGE", {
+        content: content,
+        images: images
+      });
       e.target.elements.content.value = ""; // Reset tin nhắn thành rỗng sau khi gửi
+      upload.resetPreviewPanel(); // Clear image sau khi submit
+      socket.emit("CLIENT_SEND_TYPING", "hidden");
     }
   });
 }
@@ -16,11 +32,14 @@ if (formSendData) {
 // SERVER_RETURN_MASSAGE
 socket.on("SERVER_RETURN_MASSAGE", (data) => {
   const myId = document.querySelector("[my-id]").getAttribute("my-id");
-  const chat = document.querySelector(".chat .inner-body");
+  const body = document.querySelector(".chat .inner-body");
+  const boxTyping = document.querySelector(".inner-list-typing");
 
   const div = document.createElement("div");
 
-  let htmlFullName = ``;
+  let htmlFullName = "";
+  let htmlContent = "";
+  let htmlImages = "";
 
   if (myId == data.userId) {
     div.classList.add("inner-outgoing");
@@ -29,20 +48,137 @@ socket.on("SERVER_RETURN_MASSAGE", (data) => {
     htmlFullName = `<div class="inner-name">${data.fullName}</div>`;
   }
 
+  if(data.content) {
+    htmlContent = `
+      <div class="inner-content">${data.content}</div>
+    `;
+  }
+
+  if(data.images) {
+    htmlImages += `<div class="inner-images">`;
+
+    for (const image of data.images) {
+      htmlImages += `
+        <img src="${image}">
+      `;
+    }
+    
+    htmlFullName += `
+      <img src="">
+    `;
+        
+    htmlImages += `</div>`;
+  }
+
   div.innerHTML = `
     ${htmlFullName}
-    <div class="inner-content">${data.content}</div>
+    ${htmlContent}
+    ${htmlImages}
   `;
 
-  chat.appendChild(div);
-  chat.scrollTop = chat.scrollHeight;
+  body.insertBefore(div, boxTyping);
+  body.scrollTop = body.scrollHeight;
+  
+  // preview image
+  const boxImages = div.querySelector(".inner-images");
+  if(boxImages) {
+    const gallery = new Viewer(boxImages);
+  }
 });
 // End SERVER_RETURN_MASSAGE
 
 // Scroll Chat to Bottom
 const bodyChat = document.querySelector(".chat .inner-body");
 
-if(bodyChat) {
+if (bodyChat) {
   bodyChat.scrollTop = bodyChat.scrollHeight; // scroll cách top bằng chiều cao của scroll bodyChat
 }
 // End Scroll Chat to Bottom
+
+// Show Typing
+var timeOut;
+const showTyping = () => {
+  socket.emit("CLIENT_SEND_TYPING", "show");
+  clearTimeout(timeOut);
+  timeOut = setTimeout(() => {
+    socket.emit("CLIENT_SEND_TYPING", "hidden");
+  }, 3000);
+};
+// End Show Typing
+
+// Emoji picker
+
+// Show popup icon box
+const buttonIcon = document.querySelector(".chat .inner-foot .button-icon");
+if(buttonIcon) {
+  const tooltip = document.querySelector('.tooltip');
+  Popper.createPopper(buttonIcon, tooltip);
+
+  buttonIcon.onclick = () => {
+    tooltip.classList.toggle('shown');
+  }
+}
+
+// Insert icon to input
+
+const emojiPicker = document.querySelector("emoji-picker");
+if (emojiPicker) {
+  const inputChat = document.querySelector(".chat .inner-form input[name='content']");
+
+  emojiPicker.addEventListener("emoji-click", (event) => {
+    const icon = event.detail.unicode;
+    inputChat.value = inputChat.value + icon;
+
+    // fix bị lỗi mất range khi chèn icon khi text dài 
+    const end = inputChat.value.length;
+    inputChat.setSelectionRange(end, end);
+    inputChat.focus();
+
+    showTyping();
+  });
+
+  inputChat.addEventListener("keyup", () => {
+    showTyping();
+  });
+}
+// End Emoji picker
+
+// SERVER_RETURN_TYPING
+const elementListTyping = document.querySelector(".chat .inner-list-typing");
+if(elementListTyping) {
+  socket.on("SERVER_RETURN_TYPING", (data) => {
+    if(data.type == "show") {
+      const existTyping = elementListTyping.querySelector(`[user-id="${data.userId}"]`);
+      if(!existTyping) {
+        const bodyChat = document.querySelector(".chat .inner-body");
+        const boxTyping = document.createElement("div");
+        boxTyping.classList.add("box-typing");
+        boxTyping.setAttribute("user-id", data.userId);
+        boxTyping.innerHTML = `
+          <div class="inner-name">${data.fullName}</div>
+          <div class="inner-dots"> 
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        `;
+
+        elementListTyping.appendChild(boxTyping);
+        bodyChat.scrollTop = bodyChat.scrollHeight;
+      }
+    } else {
+      const boxTypingRemove = elementListTyping.querySelector(`[user-id="${data.userId}"]`);
+      if(boxTypingRemove) {
+        elementListTyping.removeChild(boxTypingRemove);
+      }
+    }
+  });
+}
+// End SERVER_RETURN_TYPING
+
+// Preview Chat Images
+const chatBody = document.querySelector(".chat .inner-body");
+if(chatBody) {
+  const gallery = new Viewer(chatBody);
+}
+// End Preview Chat Images
